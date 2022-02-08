1d1544d81f46bde9e7e9fe2aeb854bd1b84ccf89
Ada <vesc6053@colorado.edu>
2022-02-07 18:56:42 -0700

    spinnaker driver: fix crash issue with try,except,restart; simplify arguement setting by writing automated fct for it

--- oxart/devices/spinnaker/spinnaker_pyspin_wrapper.py
+++ oxart/devices/spinnaker/spinnaker_pyspin_wrapper.py
@@ -46,7 +46,7 @@ class AllCameras:
                 cam.Init()
                 serial_nr_ptr = PySpin.CStringPtr(cam.GetTLDeviceNodeMap().GetNode('DeviceSerialNumber'))
                 serial_nr = serial_nr_ptr.GetValue()
-                self.cameras[serial_nr]=SpinnakerCamera(cam,self.lock)
+                self.cameras[serial_nr]=SpinnakerCamera(cam,self.lock,serial_nr)
                 logger.info("Initialised camera {}".format(serial_nr))
         logger.info("Initialised all cameras.")
 
@@ -78,7 +78,7 @@ class SpinnakerCamera:
     """A simple wrapper for PySpin SDK for Spinnaker cameras.
     """
 
-    def __init__(self, camera, lock_fct, framebuffer_len=100,
+    def __init__(self, camera, lock_fct, serial_nr, framebuffer_len=100,
             camera_handle=None):
         """Initialise the camera interface.
         framebuffer_len: maximum number of stored frames before oldest are discarded
@@ -86,13 +86,14 @@ class SpinnakerCamera:
         camera_handle: driver handle of camera to use. If None, assume a single camera
             system.
         """
-        self.cam = camera
+        
 
         # if camera:
         #     self.lock_camera = lambda: lock_fct
         # else:
         #     self.lock_camera = contextlib.suppress
         self.lock_camera = lock_fct
+        self.serial_nr = serial_nr
 
         # horiz = c_int()
         # vert = c_int()
@@ -121,24 +122,30 @@ class SpinnakerCamera:
 
         self.acquisition_running = False
 
+
+        self.cam = camera
+        self.node_map = self.cam.GetNodeMap()
+        self.device_node_map = self.cam.GetTLDeviceNodeMap()
+        self.stream_node_map = self.cam.GetTLStreamNodeMap()
+
     def __del__(self):
         self.close()
 
-    def _get_camera_attribute(self,attribute,node_map=0):
+    def _get_camera_attribute(self,attribute,node_map="normal"):
         ''' Read out attribute from camera, with checks whether it exists and is readable 
         node_map: 0: normal node map, 1: TL device nodemap, 2: TL Stream node map '''
-        if node_map == 0:
-            node_map = self.cam.GetNodeMap()
-        elif node_map == 1:
-            node_map = self.cam.GetTLDeviceNodeMap()
-        elif node_map == 2:
-            node_map = self.cam.GetTLStreamNodeMap()
+        if node_map == "normal":
+            node_map = self.node_map
+        elif node_map == "device":
+            node_map = self.device_node_map
+        elif node_map == "stream":
+            node_map = self.stream_node_map
         else:
-            raise Exception("Invalid argument. Node_map can only take values 0,1 or 2.")
+            raise Exception("Invalid argument. Node_map can only take values normal, device or stream.")
         node = node_map.GetNode(attribute)
         if not PySpin.IsAvailable(node):
             raise Exception("Attribute {} not available...".format(attribute))
-        if not PySpin.isReadable(node):
+        if not PySpin.IsReadable(node):
             raise Exception("Attribute {} not readable...".format(attribute))
 
         if node.GetPrincipalInterfaceType() == PySpin.intfIInteger:
@@ -152,8 +159,51 @@ class SpinnakerCamera:
         elif node.GetPrincipalInterfaceType() == PySpin.intfIString:
             return PySpin.CStringPtr(node).GetValue()
 
+    def _get_camera_attribute_int_value(self,node,value):
+        node_of_value = node.GetEntryByName(value)
+        if not PySpin.IsAvailable(node_of_value):
+            raise Exception("Value {} not available...".format(value))
+        if not PySpin.IsReadable(node_of_value):
+            raise Exception("Value {} not readable...".format(value))
+
+        int_value = node_of_value.GetValue()
+        return int_value
 
 
+    def _set_camera_attribute(self,attribute,value,node_map="normal"):
+        ''' Read out attribute from camera, with checks whether it exists and is readable 
+        node_map: 0: normal node map, 1: TL device nodemap, 2: TL Stream node map '''
+        if node_map == "normal":
+            node_map = self.node_map
+        elif node_map == "device":
+            node_map = self.device_node_map
+        elif node_map == "stream":
+            node_map = self.stream_node_map
+        else:
+            raise Exception("Invalid argument. Node_map can only take values normal, device or stream.")
+        node = node_map.GetNode(attribute)
+
+        if node.GetPrincipalInterfaceType() == PySpin.intfIInteger:
+            node_ptr = PySpin.CIntegerPtr(node)
+        elif node.GetPrincipalInterfaceType() == PySpin.intfIFloat:
+            node_ptr = PySpin.CFloatPtr(node)
+        elif node.GetPrincipalInterfaceType() == PySpin.intfIBoolean:
+            node_ptr = PySpin.CBooleanPtr(node)
+        elif node.GetPrincipalInterfaceType() == PySpin.intfIEnumeration:
+            node_ptr = PySpin.CEnumerationPtr(node)
+        elif node.GetPrincipalInterfaceType() == PySpin.intfIString:
+            node_ptr = PySpin.CStringPtr(node)
+
+        if not PySpin.IsAvailable(node_ptr):
+            raise Exception("Attribute {} not available...".format(attribute))
+        if not PySpin.IsWritable(node_ptr):
+            raise Exception("Attribute {} not writable...".format(attribute))
+
+        if type(value) is int:
+            node_ptr.SetValue(value)
+        else:
+            int_value = self._get_camera_attribute_int_value(node_ptr,value)
+            node_ptr.SetIntValue(int_value)
 
         
     def close(self):
@@ -173,11 +223,7 @@ class SpinnakerCamera:
         # with lock
         with self.lock_camera:
             if trig_mode == 'continuous':
-                node_acquisition_mode = PySpin.CEnumerationPtr(self.cam.GetNodeMap().GetNode('AcquisitionMode'))
-                node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
-
-                acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
-                node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
+                self._set_camera_attribute('AcquisitionMode','Continuous')
 
     def set_image_region(self, hStart, hEnd, vStart, vEnd, hBin=1, vBin=1):
         """Set the CCD region to read out and the horizontal and vertical
@@ -228,28 +274,20 @@ class SpinnakerCamera:
         # with self.lock_camera():
         #     self.lib.set_acquisition_mode(mode)
         #     self.lib.start_acquisition()
-        with self.lock_camera:
-            node_acquisition_mode = PySpin.CEnumerationPtr(self.cam.GetNodeMap().GetNode('AcquisitionMode'))
-            if not PySpin.IsAvailable(node_acquisition_mode) or not PySpin.IsWritable(node_acquisition_mode):
-                print('Unable to set acquisition mode to continuous (node retrieval;). Aborting... \n')
-                return False
 
-            node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
-            if not PySpin.IsAvailable(node_acquisition_mode_continuous) or not PySpin.IsReadable(
-                    node_acquisition_mode_continuous):
-                print('Unable to set acquisition mode to continuous (entry \'continuous\' retrieval). \
-                Aborting... \n')
-                return False
-
-            acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
-
-            node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
-
-        print('Camera acquisition mode set to continuous...' )
+        
+        if self.acquisition_running:
+            logger.info("Acquisition already in progress.")
+            return
 
         logger.info("Starting acquisition.")
-        if self.acquisition_running:
-            return
+        with self.lock_camera:
+            self._set_camera_attribute('StreamBufferCountMode','Manual',node_map="stream")
+            self._set_camera_attribute('StreamBufferCountManual',15,node_map="stream")
+            self._set_camera_attribute('StreamBufferHandlingMode','NewestFirst',node_map="stream")
+            self._set_camera_attribute('AcquisitionMode','Continuous')
+        logger.info('Camera acquisition mode set to continuous...' )
+
         self.acquisition_running = True
         with self.lock_camera:
             self.cam.BeginAcquisition()
@@ -257,6 +295,7 @@ class SpinnakerCamera:
     def stop_acquisition(self):
         """Stop a repeated acquisition"""
         if not self.acquisition_running:
+            logger.info("Acquisition was not running.")
             return
         self.acquisition_running = False
         with self.lock_camera:
@@ -275,21 +314,36 @@ class SpinnakerCamera:
     def _grab_image(self):
         """Returns next image in the camera buffer as a numpy
         array, or None if no new images"""
+        restart_camera = False
         with self.lock_camera:
             if self.cam.IsStreaming():
                 try:
                     image = self.cam.GetNextImage(1000)
-                    img_array = image.GetNDArray()
-                except:
-                    print(self.cam.IsInitialized())
-                    print('Missed an image.')
-                    img_array = []
+                    if image.IsIncomplete():
+                        logger.info("Image incomplete with image status {}...".format(image.GetImageStatus()))
+                        img_array = None
+                        image.Release()
+                    else:
+                        img_array = image.GetNDArray()
+                        image.Release()
+                except Exception as e:
+                    logger.info('Camera {} is initialised: {}'.format(self.serial_nr,self.cam.IsInitialized()))
+                    logger.info('Missed an image, exception occured: \n {}'.format(e))
+                    img_array = None
+                    restart_camera = True
+                    #time.sleep(1)
+                    #self.stop_acquisition()
+                    #self.start_acquisition()
             else:
-                print('Camera not streaming.')
-                img_array = []
+                logger.info('Camera not streaming.')
+                img_array = None
         #logger.info(np.shape(image.GetNDArray()))
+        if restart_camera:
+            logger.info('Restarting camera acquistion...')
+            self.stop_acquisition()
+            self.start_acquisition()
         return img_array
-        #return []
+
 
     def register_callback(self, f):
         """Register a function to be called from the acquisition thread for each
@@ -308,7 +362,7 @@ class SpinnakerCamera:
                 break
             #logger.info(self.acquisition_running)
             if self.acquisition_running:
-                im= self._grab_image()
+                im = self._grab_image()
             else:
                 im = None
             if im is None:
